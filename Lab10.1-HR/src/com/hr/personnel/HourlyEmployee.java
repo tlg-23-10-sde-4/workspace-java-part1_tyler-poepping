8786ee99d13070294473f5909c4abc837e953cb9
SSPENELOPE <typoepping@gmail.com>
2023-10-26 17:13:23 -0400

    Lab08.3 Polymorphism and Lab08.4 Abstract classes completed

--- Lab10.1-HR/src/com/hr/personnel/HourlyEmployee.java
+++ Lab10.1-HR/src/com/hr/personnel/HourlyEmployee.java
@@ -8,6 +8,8 @@
 
 package com.hr.personnel;
 
+import gov.irs.IllegalWageException;
+
 import java.time.LocalDate;
 
 public class HourlyEmployee extends Employee {
@@ -23,11 +25,14 @@ public class HourlyEmployee extends Employee {
         super(name, hireDate);  // delegate to superclass ctor for name, hireDate
     }
 
-    public HourlyEmployee(String name, LocalDate hireDate, double rate, double hours) {
+    public HourlyEmployee(String name, LocalDate hireDate, double rate, double hours)
+            throws IllegalWageException {
         this(name, hireDate);   // delegate to neighboring ctor for name, hireDate
         setRate(rate);          // handle rate here, by delegating to setter
         setHours(hours);        // handle hours here, by delegating to setter
     }
+
+
 
     // business methods
     @Override
@@ -46,9 +51,9 @@ public class HourlyEmployee extends Employee {
         return rate;
     }
 
-    public void setRate(double rate) throws IllegalArgumentException {
-        if(rate < FEDERAL_MINIMUM_WAGE) {
-            throw new IllegalArgumentException("Federal Minimum Wage rate is " + FEDERAL_MINIMUM_WAGE);
+    public void setRate(double rate) throws IllegalWageException {
+        if (rate < FEDERAL_MINIMUM_WAGE) {
+            throw new IllegalWageException("Federal Minimum Wage rate is " + FEDERAL_MINIMUM_WAGE);
         }
         this.rate = rate;
     }
